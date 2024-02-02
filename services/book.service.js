1929017e37314210a59ed49cb4a5722348e534e9
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-02-02 15:18:14 +0200

    Create _getBooks() add a list of books

---
+++ services/book.service.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var gBooks
+
+getBooks()
+
+function _getBooks() {
+ gBooks = [
+  {
+   id: makeId(),
+   title: 'A Clash of Kings',
+   price: 55,
+   imgUrl: 'imgs/A-Clash-of-Kings.jpg'
+  },
+  {
+   id: makeId(),
+   title: 'The Return of the king',
+   price: 100,
+   imgUrl: 'imgs/The-Return-of-the-King.jpg'
+  },
+  {
+   id: makeId(),
+   title: 'Harry Potter and the Order of the Phoenix',
+   price: 75,
+   imgUrl: 'imgs/Harry-Potter.jpg'
+  },
+
+ ]
+ return gBooks
+}
