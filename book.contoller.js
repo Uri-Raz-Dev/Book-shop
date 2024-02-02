61fe61384be661cbc590d3c0f9719916fa9ded1e
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-02-02 16:54:15 +0200

    Implement render() and render book table

--- book.contoller.js
+++ book.contoller.js
@@ -1,5 +1,24 @@
 'use strict'
 
 function onInit() {
-
+ render()
 }
+
+function render() {
+ const elBookList = document.querySelector('.book-table')
+ const books = _getBooks()
+ const strHtmls = books.map(book =>
+  ` <thead>
+      <tr class="title">
+        <td>${book.title}</td>
+        <td>${book.price}</td>
+        <td>
+          <button class="read" onclick="onRead(this)">Read</button>
+          <button class="update" onclick="onUpdate(this)">Update</button>
+          <button class="delete" onclick="onDelete(this)">Delete</button>
+        </td>
+      </tr>`
+ )
+ elBookList.innerHTML = strHtmls.join('')
+}
+
--- services/book.service.js
+++ services/book.service.js
@@ -2,7 +2,7 @@
 
 var gBooks
 
-getBooks()
+_getBooks()
 
 function _getBooks() {
  gBooks = [
