e118fd4c8f01255e4ee6b30cf9feb46ddccebf85
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-02-03 13:03:15 +0200

    Add to local stoarge and save

--- services/book.service.js
+++ services/book.service.js
@@ -1,26 +1,7 @@
 'use strict'
 
-var gBooks = [
- {
-  id: makeId(),
-  title: 'A Clash of Kings',
-  price: 55,
-  imgUrl: 'imgs/A-Clash-of-Kings.jpg'
- },
- {
-  id: makeId(),
-  title: 'The Return of the king',
-  price: 100,
-  imgUrl: 'imgs/The-Return-of-the-King.jpg'
- },
- {
-  id: makeId(),
-  title: 'Harry Potter and the Order of the Phoenix',
-  price: 75,
-  imgUrl: 'imgs/Harry-Potter.jpg'
- },
-
-]
+var gBooks
+_createBooks()
 
 function _getBooks() {
  return gBooks
@@ -29,7 +10,7 @@ function _getBooks() {
 function removeBook(bookId) {
  const book = gBooks.findIndex(book => book.id === bookId)
  gBooks.splice(book, 1)
-
+ _saveBooks()
 }
 
 
@@ -43,6 +24,8 @@ function updatePrice(bookID) {
   }
  }
  )
+ _saveBooks()
+
  return updatePrice
 }
 
@@ -59,11 +42,49 @@ function addBook() {
  if (isNaN(newBook.price) || newBook.price === '' || newBook.name === '' || newBook.price === null || newBook.name === null) return alert('Invalid input!')
 
  gBooks.unshift(newBook)
+ _saveBooks()
+
 
 }
 
 function readBook(bookId) {
 
  const book = gBooks.find(book => book.id === bookId)
+ _saveBooks()
+
  return book
+}
+
+
+function _createBooks() {
+ gBooks = loadFromStorage('booksDB')
+ if (!gBooks || gBooks.length === 0) {
+
+  gBooks = [
+   {
+    id: makeId(),
+    title: 'A Clash of Kings',
+    price: 55,
+    imgUrl: 'imgs/A-Clash-of-Kings.jpg'
+   },
+   {
+    id: makeId(),
+    title: 'The Return of the king',
+    price: 100,
+    imgUrl: 'imgs/The-Return-of-the-King.jpg'
+   },
+   {
+    id: makeId(),
+    title: 'Harry Potter and the Order of the Phoenix',
+    price: 75,
+    imgUrl: 'imgs/Harry-Potter.jpg'
+   },
+
+  ]
+  _saveBooks()
+ }
+}
+
+function _saveBooks() {
+ saveToStorage('booksDB', gBooks)
 }
